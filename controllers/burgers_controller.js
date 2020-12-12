0212c29d8e90377ba9e2b55f816f2e5d75f0eb8d
tony913 <tonycrosby96@gmail.com>
2020-12-12 14:02:00 -0600

    updating orm and controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,7 @@ const burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
  // all
- burger.selectAll(function (data) {
+ burger.select(function (data) {
   const burgerObject = {
       burger: data
   };
@@ -19,7 +19,7 @@ router.get("/", (req, res) => {
 
 router.get("/api/burgers", (req, res) => {
   // create
-  burger.selectAll(function (data) {
+  burger.select(function (data) {
     const burgerObject = {
         burger: data
     };
@@ -31,7 +31,7 @@ router.get("/api/burgers", (req, res) => {
 router.post("/api/burgers", (req, res) => {
   // create
   let newBurger = req.body.burger_name;
-  burger.insertOne(newBurger, function (result) {
+  burger.insert(newBurger, function (result) {
     res.json({ result });
   });
 });
@@ -42,7 +42,7 @@ router.put("/api/burgers/:id", (req, res) =>{
 
   // update
 
-  burger.updateOne(id, (result) => {
+  burger.devour(id, (result) => {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
