ebd1a386f8da57f0c1249fab3593acda876f4899
tony913 <tonycrosby96@gmail.com>
2020-12-14 15:23:07 -0600

    fixing errors in controller!

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -47,7 +47,9 @@ router.put("/api/burgers/:id", (req, res) =>{
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
-      res.status(200).end();
+      else {
+        res.status(200).end();
+      }
     }
   );
 });
