c494b1db0ed746edd38dd99281ee257eafe9244c
tony913 <tonycrosby96@gmail.com>
2020-12-14 18:21:24 -0600

    updating git commit -m updating git commit -m updating

--- config/connection.js
+++ config/connection.js
@@ -19,11 +19,17 @@ if (process.env.JAWSDB_URL) {
     user: 'root',
     password: 'password',
     database: 'burgers_db'
-  })
-};
+  });
+}
 
 // Make connection.
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
